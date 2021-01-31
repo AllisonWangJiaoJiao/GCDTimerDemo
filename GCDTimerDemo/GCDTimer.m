bbbb48f073a269c91a19bf385ded0c4fb86f00e9
allison <2807338860@qq.com>
2021-01-31 16:14:22 +0800

    解决定时器线程同步的问题，增加seaphores锁

--- GCDTimerDemo/GCDTimer.m
+++ GCDTimerDemo/GCDTimer.m
@@ -8,12 +8,15 @@
 #import "GCDTimer.h"
 
 @implementation GCDTimer
+
 static NSMutableDictionary *timerDict;
+dispatch_semaphore_t semaphore;
 
 + (void)initialize {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         timerDict = [NSMutableDictionary dictionary];
+        semaphore = dispatch_semaphore_create(1);
     });
 }
 
@@ -22,17 +25,25 @@ static NSMutableDictionary *timerDict;
            interval:(NSTimeInterval)interval
              repeat:(BOOL)repeat
               async:(BOOL)async {
-    if (!task) return nil;
+    if (!task || start < 0 || (interval <= 0 && repeat)) return nil;
     
-    NSUInteger intIdentifier = timerDict.count;
-    //定时器唯一标识
-    NSString *timerName = [NSString stringWithFormat:@"%zd",intIdentifier];
     //队列
     dispatch_queue_t queue = async ? dispatch_get_global_queue(0, 0): dispatch_get_main_queue();
     //创建定时器
     dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
     //设置时间
     dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, start * NSEC_PER_SEC, interval * NSEC_PER_SEC);
+    
+    //加锁
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    NSUInteger intIdentifier = timerDict.count;
+    //定时器唯一标识
+    NSString *timerName = [NSString stringWithFormat:@"%zd",intIdentifier];
+    //存放到字典中
+    timerDict[timerName] = timer;
+    //解锁
+    dispatch_semaphore_signal(semaphore);
+
     //设置回调
     dispatch_source_set_event_handler(timer, ^{
         task();
@@ -42,14 +53,19 @@ static NSMutableDictionary *timerDict;
     });
     //启动执行定时器
     dispatch_resume(timer);
-    //存放到字典中
-    timerDict[timerName] = timer;
     return timerName;
 }
 
 + (void)cancleTask:(NSString *)taskName {
-    dispatch_source_cancel(timerDict[taskName]);
-    [timerDict removeObjectForKey:taskName];
+    if (taskName.length == 0) return;
+    
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    dispatch_source_t _timerName = timerDict[taskName];
+    if (_timerName) {
+        dispatch_source_cancel(_timerName);
+        [timerDict removeObjectForKey:taskName];
+    }
+    dispatch_semaphore_signal(semaphore);
 }
 
 @end
